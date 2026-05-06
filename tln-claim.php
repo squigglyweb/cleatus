7ddecbcea228c088a6dedd4c115d1e0cbf8c1758
Bryan Somers <bryan@somers.design>
2026-05-06 13:51:15 +0000

    Change params to biz/pid

--- tln-claim.php
+++ tln-claim.php
@@ -1,14 +1,14 @@
 <?php
 /*
 Plugin Name: TLN Simple Claim
-Version: 1.1
+Version: 1.2
 */
 
 add_shortcode('claim_business', 'tln_claim_func');
 
 function tln_claim_func() {
-    $biz = isset($_GET['business']) ? $_GET['business'] : '';
-    $pid = isset($_GET['place_id']) ? $_GET['place_id'] : '';
+    $biz = isset($_GET['biz']) ? $_GET['biz'] : '';
+    $pid = isset($_GET['pid']) ? $_GET['pid'] : '';
     
     if (!is_user_logged_in()) {
         return "<p>Please <a href='/wp-login.php'>log in</a> to claim a business.</p>";
@@ -32,7 +32,7 @@ function tln_claim_func() {
     $out = "<div style='background:#f8f8f8;padding:2rem;border-radius:12px;max-width:500px;'>";
     if ($biz) $out .= "<p>Claiming: <strong>$biz</strong></p>";
     $out .= "<form method='post'>";
-    $out .= "<input type='hidden' name='place_id' value='$pid'>";
+    $out .= "<input type='hidden' name='pid' value='$pid'>";
     $out .= "<p><label>Business<br><input name='business_name' value='$biz' style='width:100%;padding:0.5rem;'></label></p>";
     $out .= "<p><label>Your Name<br><input name='cname' required style='width:100%;padding:0.5rem;'></label></p>";
     $out .= "<p><label>Phone<br><input name='cphone' required style='width:100%;padding:0.5rem;'></label></p>";
