d138c36003dd8bc7c2b3e8b0298e39da1c4e6f26
Bryan Somers <bryan@squigglymarketing.com>
2026-05-28 14:47:08 +0000

    Bump version to 3.7 - fix jQuery conflict in directory

--- tln-plugin/the-local-nearbuy.php
+++ tln-plugin/the-local-nearbuy.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: TLN Plugin Bundle
 Description: Business profiles, directory, and member features for The Local NearBuy
-Version: 3.6 - Added Activities & Camps CPT
+Version: 3.7 - Fixed jQuery conflict in directory
 */
 
 // Flush rewrite rules on activation
