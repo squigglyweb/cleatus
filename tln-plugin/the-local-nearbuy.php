7b6eb86d282b61a90f9476e0ff9749f669bd0530
Bryan Somers <bryan@squigglymarketing.com>
2026-05-29 06:25:36 +0000

    Remove ad slots from free profile template

--- tln-plugin/the-local-nearbuy.php
+++ tln-plugin/the-local-nearbuy.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: TLN Plugin Bundle
 Description: Business profiles, directory, and member features for The Local NearBuy
-Version: 3.24 - Fixed potential PHP error in checkout webhook
+Version: 3.25 - Removed ad slots from free profile template
 */
 
 // Flush rewrite rules on activation
