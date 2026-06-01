bc366dcd02a4b91eeb2c10d039cac3066dd720aa
Bryan Somers <bryan@squigglymarketing.com>
2026-06-01 02:20:43 +0000

    Bump version to 4.1

--- tln-plugin/the-local-nearbuy.php
+++ tln-plugin/the-local-nearbuy.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: TLN Plugin Bundle
 Description: Business profiles, directory, and member features for The Local NearBuy
-Version: 4.0 - Full camps functionality with local JSON data
+Version: 4.1 - Full camps functionality with local JSON data
 */
 
 // Create database tables on activation
